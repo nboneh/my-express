13ab1ed6e2f07564f594ea1ee19d9183d707f21a
Nir Boneh <nboneh5921@gmail.com>
2015-02-15 00:17:40 -0700

    This should fix heroku

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ require('./routes/eventView')(app)
 require('./routes/issueList')(app)
 require('./routes/issueView')(app)
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
 
     var host = server.address().address
     var port = server.address().port
